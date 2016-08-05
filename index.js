674fd98168061a28952811de35463df64ccb735d
hedgehog1029 <omgredstone@gmail.com>
2016-08-05 17:49:21 +0100

    Added DiscordBots integration and fixed reconnect bugs

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var discordie = require("discordie"),
 	Hjson = require("hjson"),
+	request = require("request"),
 	fs = require("fs");
 
 var logger = require("./lib/logger.js"),
@@ -23,13 +24,30 @@ dispatcher.manager._.bot = function() {
 	return bot;
 }
 
+dispatcher.manager._.setPermissions(discordie.Permissions);
+data._load(sdata);
+
+modules.loader.load(config.modules, dispatcher.manager, data);
+
 bot.Dispatcher.on(discordie.Events.GATEWAY_READY, function(e) {
-	dispatcher.manager._.setPermissions(discordie.Permissions);
-	data._load(sdata);
-
-	modules.loader.load(config.modules, dispatcher.manager, data);
-
 	l.info("NekomareBot started. Username: " + bot.User.username);
+
+	request.post("https://bots.discord.pw/api/bots/187533418463100928/stats", {
+		json: true,
+		body: {
+			server_count: bot.Guilds.length
+		},
+		headers: {
+			"Authorization": config.dbots.token
+		}
+	}, function(err, res, body) {
+		if (err) {
+			l.error("There was an error posting data to DiscordBots");
+			return;
+		}
+
+		l.info("DiscordBots entry updated.");
+	})
 });
 
 bot.Dispatcher.on(discordie.Events.MESSAGE_CREATE, function(e) {
